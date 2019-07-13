5fb0a7912218ec15c868af19b1a2612ef22c206f
Whu-wxy <641645560@qq.com>
2019-07-13 14:53:16 +0800

    Update trainer.py

--- allennlp/training/trainer.py
+++ allennlp/training/trainer.py
@@ -60,7 +60,8 @@ class Trainer(TrainerBase):
                  should_log_parameter_statistics: bool = True,
                  should_log_learning_rate: bool = False,
                  log_batch_size_period: Optional[int] = None,
-                 moving_average: Optional[MovingAverage] = None) -> None:
+                 moving_average: Optional[MovingAverage] = None,
+                 email: List = None) -> None:
         """
         A trainer for doing supervised learning. It just takes a labeled dataset
         and a ``DataIterator``, and uses the supplied ``Optimizer`` to learn the weights
@@ -243,6 +244,8 @@ class Trainer(TrainerBase):
         # Enable activation logging.
         if histogram_interval is not None:
             self._tensorboard.enable_activation_logging(self.model)
+
+        self._email = email
 
     def rescale_gradients(self) -> Optional[float]:
         return training_util.rescale_gradients(self.model, self._grad_norm)
@@ -554,6 +557,9 @@ class Trainer(TrainerBase):
         if best_model_state:
             self.model.load_state_dict(best_model_state)
 
+        # send Email after train
+        self.sendEmail(str(metrics))
+
         return metrics
 
     def _save_checkpoint(self, epoch: Union[int, str]) -> None:
@@ -754,3 +760,49 @@ class Trainer(TrainerBase):
                    should_log_learning_rate=should_log_learning_rate,
                    log_batch_size_period=log_batch_size_period,
                    moving_average=moving_average)
+
+    def sendEmail(self, msg : str=None):
+        import smtplib
+        from email.mime.text import MIMEText
+        from email.utils import formataddr
+
+        my_sender = ""
+        my_pass = ""
+        my_user = ""
+        #这里我为了方便自己使用，设置如果self._email为空，向自己的邮箱发送邮件
+        if self._email == None:     
+            my_sender = '641645560@qq.com'
+            my_pass = 'aockayjisawzbfib'   #这个号码是配置邮箱SMTP时得到的
+            my_user = '641645560@qq.com'
+        elif len(self._email) == 2:
+            my_sender = self._email[0]    # 发件人邮箱账号
+            my_pass = self._email[1]      # 发件人邮箱密码
+            my_user = self._email[0]      # 收件人邮箱账号，我这里发送给自己
+        elif len(self._email) == 1:
+            logger.info("lack email parameter! It consists of sender address and passward.")
+            return
+
+        if msg == None:
+            msg = "finish train"
+
+        def mail():
+            ret=True
+            try:
+                msgsend=MIMEText(msg,'plain','utf-8')
+                msgsend['From']=formataddr(["wxy",my_sender])  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
+                msgsend['To']=formataddr(["name",my_user])     # 括号里的对应收件人邮箱昵称、收件人邮箱账号
+                msgsend['Subject']="AllenNLP Result"         # 邮件的主题，也可以说是标题
+
+                server=smtplib.SMTP_SSL("smtp.qq.com", 465)  # 发件人邮箱中的SMTP服务器，端口是25
+                server.login(my_sender, my_pass)  # 括号中对应的是发件人邮箱账号、邮箱密码
+                server.sendmail(my_sender,[my_user,],msgsend.as_string())  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
+                server.quit()  # 关闭连接
+            except Exception:  # 如果 try 中的语句没有执行，则会执行下面的 ret=False
+                ret=False
+            return ret
+
+        ret=mail()
+        if ret:
+            logger.info("email successed")
+        else:
+            logger.info("email failed")
